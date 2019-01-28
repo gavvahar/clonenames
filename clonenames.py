28a71358f5038840f7165e6af9406df76355e7aa
Brendan Cazier <520246+cazier@users.noreply.github.com>
2019-01-28 10:39:39 -0600

    Initial commit

---
+++ clonenames.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+wordlists = {
+    u'Codenames': u'codenames',
+    u'Cards Against Humanity': u'cardsagainsthumanity',
+    u'Pokemon (Generation 1)': u'pokemon_gen1'}
+
+
+class Board(object):
+    """docstring for Board"""
+    def __init__(self, wordlist: str = u'codenames') -> None:
+        # self.wordlist = wordlist
+        with open(u'wordlists/{wordlist}'.format(wordlist = wordlist), u'r') as text_file:
+            self.source = [line for line in text_file.read().split(u'\n') if line != u'']
+
+    def load_settings(self, teams: int = 2, size: int = 25) -> None:
+        self.check_size(size)
+
+        self.teams = teams
+
+        self.load_words()
+
+    def check_size(self, size: int) -> None:
+        MIN = 25
+        MAX = 100
+
+        if size > MAX:
+            self.size = MAX
+
+        elif (size ** 0.5) % 1 == 0:
+            self.size = size if size > MIN else MIN
+
+        elif size < MIN:
+            self.size = MIN
+
+        else:
+            self.size = int(size ** 0.5) ** 2
+
+        self.length = int(self.size ** 0.5)
+
+    def load_words(self) -> None:
+        self.COLORS = [u'btn-red-team', u'btn-blue-team', u'btn-green-team', u'btn-yellow-team']
+        random.shuffle(self.COLORS)
+
+        self.words: list = list()
+
+        source = list(enumerate(random.sample(self.source, self.size)))
+
+        first_team_cards = int(self.size / (self.teams + 1)) + 1
+        other_team_cards = first_team_cards - 1
+        bystanders_cards = self.size - first_team_cards - (other_team_cards * (self.teams - 1)) - 1
+
+        self.FIRST = self.COLORS.pop()
+
+        for _ in range(first_team_cards):
+            self.words.append(Card(source.pop(0), self.FIRST))
+
+        for team in range(self.teams - 1):
+            color = self.COLORS.pop()
+            for _ in range(other_team_cards):
+                self.words.append(Card(source.pop(0), color))
+
+        for _ in range(bystanders_cards):
+            self.words.append(Card(source.pop(0), u'btn-bystander'))
+
+        self.words.append(Card(source.pop(0), u'btn-assassin'))
+
+        random.shuffle(self.words)
+
+        self.legend = {j.number: i for i, j in enumerate(self.words)}
+
+    def table(self) -> list:
+        return [self.words[i: i + self.length] for i in range(0, self.size, self.length)]
+
+    def get(self, entry):
+        return self.words[self.legend[int(entry)]].get()
+
+    # def statistics(self):
+    #     words = [pack for pack, file in wordlists.items() if file == self.wordlist][0]
+    #     return [self.teams, len(self.words), words]
+
+
+class Card(object):
+    def __init__(self, word: tuple, team: str) -> None:
+        self.word = word[1]
+        self.number = word[0]
+        self.team = team
+        self.shown = False
+
+    def __repr__(self) -> str:
+        return self.word
+
+    def get(self) -> dict:
+        self.shown = True
+        return {u'word': self.word, u'team': self.team}
---
+++ web.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, join_room
+
+import random
+
+import clonenames
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+games = dict()
+
+
+@app.route(u'/')
+def home_page():
+    return render_template(u'index.html')
+
+
+@app.route(u'/start', methods = [u'GET', u'POST'])
+def start_page():
+    if request.method == u'POST':
+        game = clonenames.Board(request.form.get(u'words'))
+        game.load_settings(
+            teams = int(request.form.get(u'number')),
+            size = int(request.form.get(u'size')))
+
+        room = generate_room_code()
+
+        games[room] = game
+
+        return redirect(url_for(u'game_page', room = room))
+
+    elif request.method == u'GET':
+        return render_template(u'start.html', words = clonenames.wordlists)
+
+
+@app.route(u'/game', methods = [u'GET', u'POST'])
+def game_page():
+    if request.args.get(u'room'):
+        room = request.args.get(u'room')
+        return render_template(
+            u'game.html',
+            show_input = False,
+            show_code = True,
+            room = room,
+            host = True,
+            words = games[room].table())
+
+    else:
+        try:
+            if request.method == u'GET':
+                return render_template(
+                    u'game.html',
+                    show_input = True)
+
+            elif request.method == u'POST':
+                room = request.form.get(u'room', False).upper()
+                if check_room_code(room):
+                    game = games[room]
+
+                    return render_template(
+                        u'game.html',
+                        show_input = False,
+                        show_code = True,
+                        room = room,
+                        host = request.form.get(u'host', False) == u'on',
+                        words = game.table())
+
+                else:
+                    return render_template(
+                        u'game.html',
+                        show_input = True,
+                        show_room_alert = True)
+
+        except AttributeError:
+            return redirect(url_for(u'home_page'))
+
+# @app.route(u'/statistics')
+# def stats_page():
+#     return render_template(u'statistics.html',
+#         rooms = [games[game].statistics() for game in games])
+
+
+@socketio.on(u'join')
+def join(data):
+    join_room(data[u'room'])
+
+
+@socketio.on(u'clicked')
+def handle_host_click(json):
+    room_code = json[u'room']
+
+    socketio.emit(u'revealed', {
+        u'text': u'Host clicked on {word}'.format(
+            word = games[room_code].get(json['id'])[u'word']),
+        u'id': u'#{id}'.format(id = json[u'id']),
+        u'class': games[room_code].get(json['id'])[u'team']}, room = json['room'])
+
+
+def generate_room_code():
+    letters = u''.join(random.sample(list(u'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 5))
+    while letters in games.keys():
+        return generate_room_code()
+
+    return letters
+
+
+def check_room_code(code):
+    return code in games.keys()
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host=u'0.0.0.0')
